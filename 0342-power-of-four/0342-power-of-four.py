021520f7c7f2c71b4160480e75eeff38c01ca4c8
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-11 18:29:31 +0300

    Time: 2 ms (19.85%), Space: 12.6 MB (10.27%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -5,14 +5,25 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n<= 0:
-            return False
+        # if n<= 0:
+        #     return False
 
-        log_val = math.log(n,4)//1
+        # log_val = math.log(n,4)//1
 
-        if 4** log_val == n:
-            return True
-        return False
+        # if 4** log_val == n:
+        #     return True
+        # return False
+
+        def poweroffour(n):
+
+            if n == 1:
+                return True
+            if n <= 0 or n % 4 != 0:
+                return False
+            return poweroffour(n//4)
+
+        return poweroffour(n)
+
 
 
 

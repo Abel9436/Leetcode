e28ce35f05b6d783e72be00e448e11575ca0a5a1
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-11 19:29:27 +0300

    Time: 0 ms (100%), Space: 12.4 MB (60.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,5 +5,20 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        return x ** n
+        # return x ** n
+        def power(x,n):
+            if n == 0 :
+                return 1
+            
+            if n < 0:
+                return 1 / power(x,-n)
+            
+            if n % 2  == 0:
+                half = power(x,n // 2)
+                return half * half
+            else:
+                return x * power(x,n-1)
+
+        return power(x,n)
+
         

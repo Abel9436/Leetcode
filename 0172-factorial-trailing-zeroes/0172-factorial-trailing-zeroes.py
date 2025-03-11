ae5e0933b03fd49cc70fca2bed8bd522bca7d517
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-11 18:13:30 +0300

    Time: 0 ms (100%), Space: 12.6 MB (35.78%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        #with iterative method
         # counter = 0
         # while n >= 5:
         #     n //=5
         #     counter +=n
         # return counter
 
-
-
+        
+        #with recursive method
         def trailings(n):
             ans = 0
             if n < 5:

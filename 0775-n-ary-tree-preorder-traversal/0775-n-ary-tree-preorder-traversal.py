733e50d36f739b5ec941ef980615a39538d59c91
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-19 10:55:21 +0300

    Time: 43 ms (12.05%), Space: 15.7 MB (14.46%) - LeetHub

---
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return []
+        res = [root.val]
+        for child in root.children:
+            res.extend(self.preorder(child))
+        
+        return res
+        
+        

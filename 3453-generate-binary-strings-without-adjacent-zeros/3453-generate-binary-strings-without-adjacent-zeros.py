fbdb9a2101a389d4a828b95cc12f319c28f36004
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-21 19:22:10 +0300

    Time: 47 ms (93.87%), Space: 19.9 MB (27.41%) - LeetHub

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        res = []
+        def backtrack(curr):
+            if len(curr) >= n:
+                res.append(curr[:])
+                return
+            
+            backtrack(curr + "1")
+
+            if not curr or curr[-1] != "0":
+                backtrack(curr + "0")
+        backtrack("")
+
+        return res
+
+
+        

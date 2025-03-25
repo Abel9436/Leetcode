2aa21f2847f47ddccaf9112ce6f2e4e2b4420287
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-25 17:54:51 +0300

    Time: 0 ms (100%), Space: 19.2 MB (21.12%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,37 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = last = -1
+        # first = last = -1
         
-        # Find the first occurrence
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                first = mid
-                right = mid - 1  
+        # # Find the first occurrence
+        # left, right = 0, len(nums) - 1
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] == target:
+        #         first = mid
+        #         right = mid - 1  
                 
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+        #     elif nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
         
-        # Find the last occurrence
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                last = mid
-                left = mid + 1  
+        # # Find the last occurrence
+        # left, right = 0, len(nums) - 1
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] == target:
+        #         last = mid
+        #         left = mid + 1  
 
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+        #     elif nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
         
-        return [first, last]
+        # return [first, last]
+
+        first =  bisect.bisect_left(nums,target)
+        last = bisect.bisect_right(nums,target)
+
+        return [-1,-1] if first == last else [first,last -1]
+

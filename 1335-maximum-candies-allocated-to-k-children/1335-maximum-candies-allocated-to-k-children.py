d5dc304039614e540b74e1bbe0ff2bf06beee059
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-03-27 18:03:07 +0300

    Time: 429 ms (25.81%), Space: 29.7 MB (47.96%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -2,13 +2,11 @@ class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
         if sum(candies) < k:
             return 0
-        if candies == [1]:
-            return 1
-
-        left , right = 0 , max(candies) -1
+       
+        left , right = 1 , max(candies) 
         ans = 0
 
-        while left < right:
+        while left <= right:
             mid  = (left + right) // 2
             
             healper = sum((candy // mid) for candy in candies)

b3700aaeaea753208d5705d19f9d4b5744025f2d
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-04-15 18:06:45 +0300

    Time: 31 ms (0.1%), Space: 18 MB (16.74%) - LeetHub

---
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        ## DFS
+        # value = root.val
+
+        # def dfs(node):
+        #     if not node:
+        #         return True
+            
+
+        #     if value != node.val:
+        #         return False
+
+        #     l = dfs(node.left)
+        #     r = dfs(node.right)
+        #     return l and r
+
+    
+        # return dfs(root)
+
+        ###BFS
+
+        adj = defaultdict(list)
+        def get_adj(node):
+            if not node:
+                return
+            
+            if node.left:
+                adj[node].append(node.left)
+                get_adj(node.left)
+            if node.right:
+                adj[node].append(node.right)
+                get_adj(node.right)
+
+        get_adj(root)
+        print(adj)
+            
+
+        # for 
+        def bfs(graph, node):
+
+
+            visited = set([node])
+            queue= deque([node])
+            _list = []
+
+            while queue:
+                node = queue.popleft()
+
+                _list.append(node.val)
+
+                for nei in graph[node]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        queue.append(nei)
+            
+            return _list
+        
+        
+        
+        return len(set(bfs(adj,root))) == 1
+
+
+
+
+

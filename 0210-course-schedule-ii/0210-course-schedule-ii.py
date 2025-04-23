97bdc594927ac77ae43b317c64a76acf8f5053ba
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2025-04-23 18:40:33 +0300

    Time: 3 ms (77.12%), Space: 19.1 MB (72.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,31 +2,69 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
 
-        graph = [[] for _ in range(numCourses)]
-        indegree = [0 for _ in range(numCourses)]
-        queue = deque()
-        order = []
+        # graph = defaultdict(list)
+        # indegree = [0 for _ in range(numCourses)]
+        # queue = deque()
+        # order = []
         
+
+        # for c, p in prerequisites:
+
+        #     graph[p].append(c)
+
+        #     indegree[c] += 1
+        
+        # for d in range(numCourses):
+        #     if indegree[d] == 0:
+        #         queue.append(d)
+        
+        # while queue:
+        #     node = queue.popleft()
+
+        #     for nei in graph[node]:
+        #         indegree[nei] -=1
+        #         if indegree[nei] == 0:
+        #             queue.append(nei)
+        #     order.append(node)
+        # if len(order) != numCourses:
+        #     return []
+        
+        # return order
+
+
+
+        ############
+
+        #let us solve it by dfs
+
+        graph = defaultdict(list)
 
         for c, p in prerequisites:
 
             graph[p].append(c)
+         
+        visited = [0] * numCourses
 
-            indegree[c] += 1
-        
-        for d in range(numCourses):
-            if indegree[d] == 0:
-                queue.append(d)
-        
-        while queue:
-            node = queue.popleft()
+        stack = []
 
-            for nei in graph[node]:
-                indegree[nei] -=1
-                if indegree[nei] == 0:
-                    queue.append(nei)
-            order.append(node)
-        if len(order) != numCourses:
-            return []
+        def dfs(node):
+
+            visited[node] = 1
+
+            for nei in graph.get(node , []):
+                if visited[nei] == 1:
+                    return True
+                if visited[nei] == 0:
+                    if dfs(nei):
+                        return True
+            visited[node] = 2
+            stack.append(node)
+            return False
         
-        return order
+        for node in range(numCourses):
+            if visited[node] == 0:
+                if dfs(node):
+                    return []
+
+        return stack[::-1]
+
